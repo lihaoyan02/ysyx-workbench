994e0fbce4ad1839d29bccc6f656437e72a1201c
tracer-ysyx <tracer@ysyx.org>
2025-12-08 19:14:49 +0800

    > sim RTL ysyx_22040000 Li Haoyan Linux ysyx 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:14:49 up 15 min,  4 users,  load average: 0.36, 0.28, 0.17

--- npc/csrc/tb.cpp
+++ npc/csrc/tb.cpp
@@ -3,17 +3,26 @@
 #include <assert.h>
 
 #include <verilated.h>
-#include "Vtop.h"
+#include <Vtop.h>
 #include "verilated_fst_c.h"
 
+#include <nvboard.h>
+
+static TOP_NAME top;
+void nvboard_bind_all_pins(TOPNAME* top);
 int main(int argc, char **argv){
+
 	VerilatedContext* const contextp = new VerilatedContext;
 	contextp->commandArgs(argc, argv);
 	Vtop* const top = new Vtop{contextp};
+	
 	Verilated::traceEverOn(true);
 	VerilatedFstC* tfp = new VerilatedFstC;
 	top->trace(tfp, 1);
 	tfp->open("obj_dir/wave.fst");
+	
+	nvboard_blind_all_pins(&top);
+	nvboard_init();
 	int cnt=0;
 	while(cnt<=50){
 		int a = rand() & 1;
@@ -25,6 +34,7 @@ int main(int argc, char **argv){
 		printf("a = %d, b = %d, f = %d\n", a, b, top->f);
 		assert(top->f == (a ^ b));
 		cnt++;
+		nvboard_update();
 	}
 	tfp->close();
 	delete top;
