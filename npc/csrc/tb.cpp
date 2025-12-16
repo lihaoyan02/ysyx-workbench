a8c589ead7c61ae59b0c751d5ce1b21c84d12771
tracer-ysyx <tracer@ysyx.org>
2025-12-16 20:46:39 +0800

    > compile NEMU ysyx_22040000 Li Haoyan Linux ysyx 6.8.0-88-generic #89~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 29 10:45:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:46:39 up  9:52,  5 users,  load average: 0.01, 0.00, 0.00

--- npc/csrc/tb.cpp
+++ npc/csrc/tb.cpp
@@ -4,39 +4,39 @@
 
 #include <verilated.h>
 #include <Vtop.h>
-#include "verilated_fst_c.h"
+//#include "verilated_fst_c.h"
 
 #include <nvboard.h>
 
-static TOP_NAME top;
-void nvboard_bind_all_pins(TOPNAME* top);
+static TOP_NAME dut;
+void nvboard_bind_all_pins(TOP_NAME* top);
 int main(int argc, char **argv){
 
-	VerilatedContext* const contextp = new VerilatedContext;
-	contextp->commandArgs(argc, argv);
-	Vtop* const top = new Vtop{contextp};
+	//VerilatedContext* const contextp = new VerilatedContext;
+	//contextp->commandArgs(argc, argv);
+	//Vtop* const top = new Vtop{contextp};
 	
-	Verilated::traceEverOn(true);
-	VerilatedFstC* tfp = new VerilatedFstC;
-	top->trace(tfp, 1);
-	tfp->open("obj_dir/wave.fst");
+	//Verilated::traceEverOn(true);
+	//VerilatedFstC* tfp = new VerilatedFstC;
+	//top->trace(tfp, 1);
+	//tfp->open("obj_dir/wave.fst");
 	
-	nvboard_blind_all_pins(&top);
+	nvboard_bind_all_pins(&dut);
 	nvboard_init();
 	int cnt=0;
 	while(cnt<=50){
-		int a = rand() & 1;
-		int b = rand() & 1;
-		top->a = a;
-		top->b = b;
-		top->eval();
-		tfp->dump(cnt);
-		printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-		assert(top->f == (a ^ b));
-		cnt++;
+		//int a = rand() & 1;
+		//int b = rand() & 1;
+		//top->a = a;
+		//top->b = b;
+		dut.eval();
+		//tfp->dump(cnt);
+		//printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+		//assert(top->f == (a ^ b));
+		//cnt++;
 		nvboard_update();
 	}
-	tfp->close();
-	delete top;
+	//tfp->close();
+	//delete top;
 	return 0;
 }
