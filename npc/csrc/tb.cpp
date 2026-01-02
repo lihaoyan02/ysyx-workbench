fb623884b424aefaa4d65a3b01a70ae20d624d26
tracer-ysyx <tracer@ysyx.org>
2026-01-02 15:06:42 +0800

    > sim RTL ysyx_22040000 Li Haoyan Linux ysyx 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:06:42 up  5:37,  4 users,  load average: 0.12, 0.16, 0.06

--- npc/csrc/tb.cpp
+++ npc/csrc/tb.cpp
@@ -7,37 +7,50 @@
 //#include "verilated_fst_c.h"
 
 
-static Vtop top;
+static uint8_t pmem[1000];
+static const uint32_t img[] = {
+	0x01400513, 0x010000e7, 0x00c000e7, 0x00c00067,
+	0x00a50513, 0x00008067
+};
 
-static void single_cycle() { 
-	top.clk = 0; top.eval();
-	top.clk = 1; top.eval();
+static uint32_t pmem_read(uint32_t addr, int len) {
+	uint8_t* paddr = pmem + addr;
+	switch (len) {
+		case 1: return *(uint8_t  *)paddr;
+		case 2: return *(uint16_t *)paddr;
+		case 4: return *(uint32_t *)paddr;
+		default: assert(0);
+	}
+}
+
+static void single_cycle(Vtop * top) { 
+	top->clk = 0; top->eval();
+	top->clk = 1; top->eval();
 }
 int main(int argc, char **argv){
 
 	VerilatedContext* const contextp = new VerilatedContext;
 	contextp->commandArgs(argc, argv);
-	//Vtop* const top = new Vtop{contextp};
+	Vtop* const top= new Vtop{contextp};
 	
 	//Verilated::traceEverOn(true);
 	//VerilatedFstC* tfp = new VerilatedFstC;
 	//top->trace(tfp, 1);
 	//tfp->open("obj_dir/wave.fst");
-	
+	memcpy(pmem, img, sizeof(img));	
 	int cnt=0;
-	while(cnt<=50){
-		//int a = rand() & 1;
-		//int b = rand() & 1;
-		//top->a = a;
-		//top->b = b;
-		single_cycle();
-		top.eval();
-		//tfp->dump(cnt);
-		//printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-		//assert(top->f == (a ^ b));
-		cnt++;
+	top->rst = 1;
+	single_cycle(top);
+	top->rst = 0;
+	while(cnt<=10){
+		top->inst = pmem_read(top->pc, 4);
+		top->eval();
+		single_cycle(top);
+		top->eval();
+		cnt++; 
+		printf("pc = %x \n",top->pc);
 	}
 	//tfp->close();
-	//delete top;
+	//delete top
 	return 0;
 }
