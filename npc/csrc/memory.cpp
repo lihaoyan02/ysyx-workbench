da79686332837dd128ebc2c6e8eedca750db69db
tracer-ysyx <tracer@ysyx.org>
2026-01-04 12:18:39 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:18:39 up  2:15,  5 users,  load average: 0.00, 0.01, 0.00

--- npc/csrc/memory.cpp
+++ npc/csrc/memory.cpp
@@ -1,28 +1,37 @@
 #include <memory.h>
 #include <assert.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <Vtop__Dpi.h>
 
 static uint8_t pmem[MEM_MAX];
 
 extern "C" int pmem_read(int raddr, int len) {
-	uint8_t* paddr = pmem + ((unsigned)raddr & ~0x3u);
-	switch (len) {
-		case 1: return *(uint8_t  *)paddr;
-		case 2: return *(uint16_t *)paddr;
-		case 4: return *(uint32_t *)paddr;
-		default: assert(0);
+	if(in_mem((uint32_t)raddr)) {
+		uint8_t* paddr = pmem + ((unsigned)raddr & ~0x3u) - MEM_BASE;
+		switch (len) {
+			case 1: return *(uint8_t  *)paddr;
+			case 2: return *(uint16_t *)paddr;
+			case 4: return *(uint32_t *)paddr;
+			default: assert(0);
+		}
+	} else {
+		printf("illegal access for pmem\n");
+		assert(0);
 	}
 }	
 
 extern "C" void pmem_write(int waddr, int wdata, char wmask) {
-	uint8_t* paddr = pmem + (unsigned)waddr;
-	switch (wmask) {
-		case 0x1: *(uint8_t *)paddr = (uint8_t)wdata; break;
-		case 0x3: *(uint16_t *)paddr = (uint16_t)wdata; break;
-		case 0xf: *(uint32_t *)paddr = wdata; break;
-		default: assert(0);
+	if(in_mem((uint32_t)waddr)) {
+		uint8_t* paddr = pmem + (unsigned)waddr - MEM_BASE;
+		switch (wmask) {
+			case 0x1: *(uint8_t *)paddr = (uint8_t)wdata; break;
+			case 0x3: *(uint16_t *)paddr = (uint16_t)wdata; break;
+			case 0xf: *(uint32_t *)paddr = wdata; break;
+			default: assert(0);
+		}
+	} else {
+		printf("illegal access for pmem\n");
+		assert(0);
 	}
 }
 
@@ -45,7 +54,7 @@ int load_mem(const char *img){
 	}
 	fclose(file);
 	//pmem_write(0x228, 0x00100073, 0b1111);
-	pmem_write(0x1220, 0x00100073, 0b1111);
+	//pmem_write(0x1220, 0x00100073, 0b1111);
 	//printf("0x1220 = %08x\n",pmem_read(0x1220,4)); 
 	return 0;
 }
--- npc/include/memory.h
+++ npc/include/memory.h
@@ -2,6 +2,14 @@
 #define __NPC_MEMORY_H__
 
 #define MEM_MAX 0x8000000 
+#define MEM_BASE 0
+
+//0x80000000 
+#include <cstdint>
+static bool in_mem(uint32_t addr) { 
+	return addr - MEM_BASE < MEM_MAX; 
+}
+
 extern "C" int pmem_read(int raddr, int len);
 
 extern "C" void pmem_write(int waddr, int wdata, char wmask);
