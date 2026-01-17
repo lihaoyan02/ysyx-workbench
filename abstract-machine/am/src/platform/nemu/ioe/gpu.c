0d48baaf0b24eebda4747e9245dd39b83d2dbb22
tracer-ysyx <tracer@ysyx.org>
2026-01-17 18:42:28 +0800

    > compile NEMU ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:42:28 up  3:58,  5 users,  load average: 0.13, 0.09, 0.07

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,12 +4,14 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+	/*
 	int i;
 	int w = inl(VGACTL_ADDR)>>16;
 	int h = inw(VGACTL_ADDR);
 	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
 	for (i = 0; i < w * h; i ++) fb[i] = i;
 	outl(SYNC_ADDR, 1);
+	*/
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -21,6 +23,11 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+	int x = ctl->x;
+	int y = ctl->y;
+	int pos = y * ctl->w + x;
+	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+	outl(fb[pos], ((uint32_t *)ctl->pixels)[pos]);
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
