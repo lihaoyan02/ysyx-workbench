c92a82d8635a60e3efb45fd5e7576e716382550a
tracer-ysyx <tracer@ysyx.org>
2026-01-17 22:18:59 +0800

    > compile NEMU ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:18:59 up  7:35,  4 users,  load average: 0.31, 0.12, 0.04

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -114,6 +114,7 @@ static int decode_exec(Decode *s) {
 	INSTPAT("0000001 ????? ????? 101 ????? 01100 11", divu 	 , R, R(rd) = (src2==0) ? -1 : src1 / src2); 
 	INSTPAT("0000001 ????? ????? 000 ????? 01100 11", mul 	 , R, R(rd) = BITS((int64_t)(int32_t)src1 * (int64_t)(int32_t)src2, 31, 0)); 
 	INSTPAT("0000001 ????? ????? 001 ????? 01100 11", mulh 	 , R, R(rd) = BITS((int64_t)(int32_t)src1 * (int64_t)(int32_t)src2, 63, 32)); 
+	INSTPAT("0000001 ????? ????? 010 ????? 01100 11", mulhsu , R, R(rd) = BITS((int64_t)(int32_t)src1 * (uint64_t)src2, 63, 32)); 
 	INSTPAT("0000001 ????? ????? 011 ????? 01100 11", mulhu  , R, R(rd) = BITS((uint64_t)src1 * (uint64_t)src2, 63, 32)); 
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
