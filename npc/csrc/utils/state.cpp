d5c15adf55a3d8f2387de3c4051351b2e6037fd9
tracer-ysyx <tracer@ysyx.org>
2026-02-10 21:07:21 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:07:21 up 30 min,  3 users,  load average: 0.01, 0.03, 0.03

--- npc/csrc/utils/state.cpp
+++ npc/csrc/utils/state.cpp
@@ -1,6 +1,6 @@
 #include <utils.h>
 
-NPCState npc_state = { .state = NPC_RUNNING };
+NPCState npc_state = { .state = NPC_STOP };
 
 int is_exit_status_bad() {
 	int good = (npc_state.state == NPC_END && npc_state.halt_ret == 0) ||
