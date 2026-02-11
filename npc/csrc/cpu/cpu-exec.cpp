22fae1108959ed18f51ec2a7422592022dc29efe
tracer-ysyx <tracer@ysyx.org>
2026-02-11 14:43:17 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:43:17 up 11 min,  3 users,  load average: 0.03, 0.10, 0.08

--- npc/csrc/cpu/cpu-exec.cpp
+++ npc/csrc/cpu/cpu-exec.cpp
@@ -66,7 +66,7 @@ static void exec_once(Decode *s) {
 	int ilen = s->snpc - s->pc;
 	int i;
 	// dpi
-	const svScope scope = svGetScopeFromName("TOP.top");
+	const svScope scope = svGetScopeFromName("TOP.top.u_IFU");
 	assert(scope);
 	svSetScope(scope);
 	uint32_t inst32 = read_inst();
