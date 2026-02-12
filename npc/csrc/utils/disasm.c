a5a0d53188bddc46efed0e837ef9b13670ba6a8b
tracer-ysyx <tracer@ysyx.org>
2026-02-12 19:49:26 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:49:26 up  4:18,  4 users,  load average: 0.16, 0.09, 0.08

--- npc/csrc/utils/disasm.c
+++ npc/csrc/utils/disasm.c
@@ -58,6 +58,7 @@ void init_disasm() {
 
  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
 	cs_insn *insn;
+	printf("111\n");
 	size_t count = cs_disasm_dl(handle, code, nbyte, pc, 0, &insn);
   assert(count == 1);
 	printf("111\n");
