c57a0d0de4160d9334cdb0d7c287bb8fceafdab7
tracer-ysyx <tracer@ysyx.org>
2026-02-23 20:11:33 +0800

    > compile NEMU ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:11:33 up  7:37,  5 users,  load average: 0.00, 0.02, 0.02

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -65,11 +65,11 @@ void free_fp();
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
+	iringbuf_push(_this->logbuf);
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 	// iringtrace
-	iringbuf_push(_this->logbuf);
 #ifdef CONFIG_FTRACE
 	ftrace_rcd(_this);
 #endif
