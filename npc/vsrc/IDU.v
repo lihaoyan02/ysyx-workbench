cdb7ee77070bd34922a6f0ba847a1bf60e471ee8
tracer-ysyx <tracer@ysyx.org>
2026-03-02 17:41:58 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:41:58 up  1:16,  5 users,  load average: 0.15, 0.08, 0.02

--- npc/vsrc/IDU.v
+++ npc/vsrc/IDU.v
@@ -104,6 +104,8 @@ localparam J_UNCOND = 2'b00, J_BEQ = 2'b01, J_BNE = 2'b10;
 					j_en = 1'b1;
 					j_cond = J_BEQ;
 				end	
+				else
+					unknow_inst(); 
 			end
 			7'b1100111: begin //jalr
 				if (funct3 == 3'b000) begin
