237c17a6d4944406d2ebafe407b1db5afc3662aa
tracer-ysyx <tracer@ysyx.org>
2026-03-24 23:32:45 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  23:32:45 up  4:21,  5 users,  load average: 0.14, 0.09, 0.04

--- npc/vsrc/IFU.v
+++ npc/vsrc/IFU.v
@@ -4,8 +4,26 @@ module IFU #(INST_WIDTH = 32, ADDR_WIDTH = 32)(
 	input j_pc,
 	input [ADDR_WIDTH-1:0] j_pc_addr,
 	output reg [ADDR_WIDTH-1:0] pc,
-	output reg [INST_WIDTH-1:0] inst_fetch
+	output reg [INST_WIDTH-1:0] inst_fetch,
+	output reg idu_en
 );
+localparam IDLE = 1'b0, WAIT = 1'b1;
+reg state, next_state;
+always @(posedge clk) begin
+	if (rst)
+		state <= IDLE;
+	else
+		state <= next_state;
+end
+
+always @(*) begin
+	case (state)
+		IDLE:
+			next_state = WAIT;
+		WAIT:
+			next_state = IDLE;
+	endcase
+end
 
 import "DPI-C" function int pmem_read(int raddr);
 reg rst_r;
@@ -15,9 +33,9 @@ assign next_pc = j_pc ? j_pc_addr : pc + 4;
 
 always @(posedge clk) begin
 	if (rst) pc <= 32'h80000000;//{ADDR_WIDTH{1'b0}}; 
-	else if(rst_r)
-		pc <= 32'h80000000;
-	else
+	//else if(rst_r)
+		//pc <= 32'h80000000;
+	else if(state==WAIT)
 		pc <= next_pc;
 end
 
@@ -25,11 +43,17 @@ always @(posedge clk) begin
 	rst_r <= rst;
 end
 
-always @(*) begin
-	if (rst)
-		inst_fetch = 32'b0;
+always @(posedge clk) begin
+	if (rst) begin
+		inst_fetch <= 32'b0;
+		idu_en <= 0;
+	end
+	else if(state==IDLE) begin
+		inst_fetch <= pmem_read(pc);
+		idu_en <= 1;
+	end
 	else
-		inst_fetch = pmem_read(pc);
+		idu_en <= 0;
 end
 
 function int read_inst();
