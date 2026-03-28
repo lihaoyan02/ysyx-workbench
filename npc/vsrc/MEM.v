0ef782d6d76028194cc1950da807080407d0b122
tracer-ysyx <tracer@ysyx.org>
2026-03-28 16:45:50 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  16:45:50 up  4:03,  4 users,  load average: 1.20, 0.84, 0.51

--- npc/vsrc/MEM.v
+++ npc/vsrc/MEM.v
@@ -12,38 +12,38 @@ module MEM #(DATA_WIDTH = 32, ADDR_WIDTH=32, SHIFT_LEN=4) (
 import "DPI-C" function int pmem_read(int raddr);
 import "DPI-C" function void pmem_write(int waddr, int wdata, byte wmask);
 
+// always @(posedge clk) begin
+//   rdata <= (reqValid && !wen) ? pmem_read(addr) : 32'b0;
+//   if (reqValid && wen) begin
+//     pmem_write(addr, wdata, {4'b0,wmask});
+//   end
+//   respValid <= reqValid;
+// end
+
 always @(posedge clk) begin
-  rdata <= (reqValid && !wen) ? pmem_read(addr) : 32'b0;
-  if (reqValid && wen) begin
-    pmem_write(addr, wdata, {4'b0,wmask});
-  end
-  respValid <= reqValid;
+    if (reqValid && !wen)
+        rdata <= pmem_read(addr);
+    // else if(respValid)
+    //     rdata <= 0;
+    if (reqValid && wen) begin
+        pmem_write(addr, wdata, {4'b0,wmask});
+    end
 end
 
-// always @(posedge clk) begin
-//     if (reqValid && !wen)
-//         rdata <= pmem_read(addr);
-//     // else if(respValid)
-//     //     rdata <= 0;
-//     if (reqValid && wen) begin
-//         pmem_write(addr, wdata, {4'b0,wmask});
-//     end
-// end
-
-// always @(posedge clk) begin
-//     if (reqValid && wen) begin
-//         respValid <= reqValid;
-//     end
-//     else
-//         respValid <= shift_reg[0];
-// end
-// reg [SHIFT_LEN-1:0] shift_reg;
-// always @(posedge clk) begin
-//     if (reqValid && !wen) begin
-//         shift_reg <= {reqValid,shift_reg[SHIFT_LEN-1:1]};
-//     end
-//     else
-//         shift_reg <= {1'b0,shift_reg[SHIFT_LEN-1:1]};
-// end
+always @(posedge clk) begin
+    if (reqValid && wen) begin
+        respValid <= reqValid;
+    end
+    else
+        respValid <= shift_reg[0];
+end
+reg [SHIFT_LEN-1:0] shift_reg;
+always @(posedge clk) begin
+    if (reqValid && !wen) begin
+        shift_reg <= {reqValid,shift_reg[SHIFT_LEN-1:1]};
+    end
+    else
+        shift_reg <= {1'b0,shift_reg[SHIFT_LEN-1:1]};
+end
 
 endmodule
