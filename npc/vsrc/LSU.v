a6acf52f69a8e91f0d4b41d7f049b66bf8baed8f
tracer-ysyx <tracer@ysyx.org>
2026-03-28 14:44:37 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  14:44:37 up  2:02,  4 users,  load average: 0.57, 0.29, 0.22

--- npc/vsrc/LSU.v
+++ npc/vsrc/LSU.v
@@ -17,8 +17,6 @@ module LSU #(DATA_WIDTH = 32, ADDR_WIDTH=32) (
 	input respValid,
 	input [DATA_WIDTH-1:0] mem_rdata
 );
-//import "DPI-C" function int pmem_read(int raddr);
-//import "DPI-C" function void pmem_write(int waddr, int wdata, byte wmask);
 
 reg state, next_state;
 localparam IDLE = 1'b0, WAIT = 1'b1;
@@ -47,24 +45,9 @@ assign reqValid = lsu_en & (state==IDLE);
 
 reg [DATA_WIDTH-1:0] rdata_word;
 reg [DATA_WIDTH-1:0] rdata_word_n;
-//reg read_out;
-
-//assign reqValid = lsu_en&ready_r;
 assign mem_wen = wen;
 assign rdata_word = mem_rdata;
 assign mem_addr = addr;
-// reg ready_r;
-// always @(posedge clk) begin
-// 	if (lsu_en & ~wen) begin
-// 		ready_r <= 0;
-// 	end
-// 	else if (respValid) begin
-// 		ready_r <= 1;
-// 	end
-// 	else
-// 		ready_r <= 1;
-// end
-// assign ready_out = ~((lsu_en & ~wen) | ~ready_r) | respValid;
 
 always @(*) begin
 	if(lsu_en&wen) begin
@@ -120,77 +103,11 @@ always @(*) begin
 		endcase
 	end
 end
-/*
-always @(posedge clk) begin
-	if(lsu_en) begin
-		if (wen) begin // write enable : store data
-			case (lsu_ctrl)
-				3'b000: begin //sb
-					case (addr[1:0])
-						2'b00: pmem_write(addr, wdata, 8'b1); 
-						2'b01: pmem_write(addr, wdata<<8, 8'b10); 
-						2'b10: pmem_write(addr, wdata<<16, 8'b100); 
-						2'b11: pmem_write(addr, wdata<<24, 8'b1000); 
-					endcase
-				end
-				3'b001: begin //sj
-					case (addr[1:0])
-						2'b00: pmem_write(addr, wdata, 8'b11); 
-						2'b01: pmem_write(addr, wdata<<8, 8'b110); 
-						2'b10: pmem_write(addr, wdata<<16, 8'b1100); 
-						2'b11: begin
-							pmem_write(addr, wdata<<24, 8'b1000); 
-							pmem_write(addr, wdata>>8, 8'b0001); 
-						end
-					endcase
-				end
-				3'b010: begin //sw
-					case (addr[1:0])
-						2'b00: pmem_write(addr, wdata, 8'b1111); 
-						2'b01: begin
-							pmem_write(addr, wdata<<8, 8'b1110); 
-							pmem_write(addr, wdata>>24, 8'b0001); 
-						end
-						2'b10: begin
-							pmem_write(addr, wdata<<16, 8'b1100); 
-							pmem_write(addr, wdata>>16, 8'b0011); 
-						end
-						2'b11: begin
-							pmem_write(addr, wdata<<24, 8'b1000); 
-							pmem_write(addr, wdata>>8, 8'b0111); 
-						end
-					endcase
-				end
-				default: $finish;
-			endcase
-		end
-	end
-end
 
-always @(posedge clk) begin
-	if(lsu_en & ~wen) begin
-		read_out <= 1;
-		case (lsu_ctrl)
-			3'b100, 3'b000: begin
-				rdata_word <= pmem_read(addr);
-				rdata_word_n <= 32'b0;
-			end
-			3'b010, 3'b101, 3'b001: begin
-				 rdata_word <= pmem_read(addr);
-				 rdata_word_n <= pmem_read(addr);
-			end
-			default: $finish;
-		endcase
-	end
-	else
-		read_out <= 0;
-end
-
-assign ready_out = ~(lsu_en & ~wen & ~read_out);
-*/
 reg [2:0] lsu_ctrl_r;
 reg [1:0] addr_r;
 reg wen_r;
+
 always @(posedge clk) begin //latch the info
 	if (rst) begin
 		lsu_ctrl_r <= 0;
