1d5c27187ac5c242f56d35367fdae1345e4cf8b0
tracer-ysyx <tracer@ysyx.org>
2026-03-28 14:34:49 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  14:34:49 up  1:52,  3 users,  load average: 0.14, 0.32, 0.24

--- npc/vsrc/LSU.v
+++ npc/vsrc/LSU.v
@@ -1,6 +1,7 @@
 module LSU #(DATA_WIDTH = 32, ADDR_WIDTH=32) (
 	input lsu_en,
 	input clk,
+	input rst,
 	input wen,
 	input [2:0] lsu_ctrl,
 	input [DATA_WIDTH-1:0] wdata,
@@ -16,29 +17,54 @@ module LSU #(DATA_WIDTH = 32, ADDR_WIDTH=32) (
 	input respValid,
 	input [DATA_WIDTH-1:0] mem_rdata
 );
-import "DPI-C" function int pmem_read(int raddr);
-import "DPI-C" function void pmem_write(int waddr, int wdata, byte wmask);
+//import "DPI-C" function int pmem_read(int raddr);
+//import "DPI-C" function void pmem_write(int waddr, int wdata, byte wmask);
+
+reg state, next_state;
+localparam IDLE = 1'b0, WAIT = 1'b1;
+
+assign ready_out = state==WAIT & respValid;
+
+always @(posedge clk) begin
+	if (rst)
+		state <= IDLE;
+	else
+		state <= next_state;
+end
+
+always @(*) begin
+	case (state)
+		IDLE: begin
+			next_state =  reqValid ? WAIT : IDLE;
+		end
+		WAIT: begin
+			next_state = respValid ? IDLE : WAIT;
+		end
+	endcase
+end
+
+assign reqValid = lsu_en & (state==IDLE);
 
 reg [DATA_WIDTH-1:0] rdata_word;
 reg [DATA_WIDTH-1:0] rdata_word_n;
-reg read_out;
+//reg read_out;
 
-assign reqValid = lsu_en&ready_r;
+//assign reqValid = lsu_en&ready_r;
 assign mem_wen = wen;
 assign rdata_word = mem_rdata;
 assign mem_addr = addr;
-reg ready_r;
-always @(posedge clk) begin
-	if (lsu_en & ~wen) begin
-		ready_r <= 0;
-	end
-	else if (respValid) begin
-		ready_r <= 1;
-	end
-	else
-		ready_r <= 1;
-end
-assign ready_out = ~((lsu_en & ~wen) | ~ready_r) | respValid;
+// reg ready_r;
+// always @(posedge clk) begin
+// 	if (lsu_en & ~wen) begin
+// 		ready_r <= 0;
+// 	end
+// 	else if (respValid) begin
+// 		ready_r <= 1;
+// 	end
+// 	else
+// 		ready_r <= 1;
+// end
+// assign ready_out = ~((lsu_en & ~wen) | ~ready_r) | respValid;
 
 always @(*) begin
 	if(lsu_en&wen) begin
@@ -162,40 +188,53 @@ end
 
 assign ready_out = ~(lsu_en & ~wen & ~read_out);
 */
+reg [2:0] lsu_ctrl_r;
+reg [1:0] addr_r;
+reg wen_r;
+always @(posedge clk) begin //latch the info
+	if (rst) begin
+		lsu_ctrl_r <= 0;
+		wen_r <= 0;
+		addr_r <= 0;
+	end
+	else if (reqValid & state==IDLE) begin
+		lsu_ctrl_r <= lsu_ctrl;
+		wen_r <= wen;
+		addr_r <= addr[1:0];
+	end
+end
 always @(*) begin
 	rdata = 32'b0;
-	if(lsu_en) begin
-		if (~wen) begin // write enable : store data
-			case (lsu_ctrl)
-				3'b100: rdata = addr[1:0]==2'b00 ? {24'b0, rdata_word[7:0]} :
-												addr[1:0]==2'b01 ? {24'b0, rdata_word[15:8]} :
-												addr[1:0]==2'b10 ? {24'b0, rdata_word[23:16]} :
-												addr[1:0]==2'b11 ? {24'b0, rdata_word[31:24]} : 32'b0; //lbu 
-				3'b000: rdata = addr[1:0]==2'b00 ? {{24{rdata_word[7]}}, rdata_word[7:0]} :
-												addr[1:0]==2'b01 ? {{24{rdata_word[15]}}, rdata_word[15:8]} :
-												addr[1:0]==2'b10 ? {{24{rdata_word[23]}}, rdata_word[23:16]} :
-												addr[1:0]==2'b11 ? {{24{rdata_word[31]}}, rdata_word[31:24]} : 32'b0; //lb 
-				3'b010: begin //lw
-					rdata = addr[1:0]==2'b00 ? rdata_word : 
-									addr[1:0]==2'b01 ? {rdata_word_n[7:0], rdata_word[31:8]} :
-									addr[1:0]==2'b10 ? {rdata_word_n[15:0], rdata_word[31:16]} :
-									addr[1:0]==2'b11 ? {rdata_word_n[23:0], rdata_word[31:24]} : 32'b0;
-				end
-				3'b101: begin //lhu 
-					rdata = addr[1:0]==2'b00 ? {16'b0, rdata_word[15:0]} :
-									addr[1:0]==2'b01 ? {16'b0, rdata_word[23:8]} :
-									addr[1:0]==2'b10 ? {16'b0, rdata_word[31:16]} :
-									addr[1:0]==2'b11 ? {16'b0, rdata_word_n[7:0], rdata_word[31:24]} : 32'b0;  
-				end
-				3'b001: begin //lh 
-					rdata = addr[1:0]==2'b00 ? {{16{rdata_word[15]}}, rdata_word[15:0]} :
-									addr[1:0]==2'b01 ? {{16{rdata_word[23]}}, rdata_word[23:8]} :
-									addr[1:0]==2'b10 ? {{16{rdata_word[31]}}, rdata_word[31:16]} :
-									addr[1:0]==2'b11 ? {{16{rdata_word_n[7]}},rdata_word_n[7:0], rdata_word[31:24]} : 32'b0;  
-				end
-				default: $finish;
-			endcase
-		end
+	if (respValid & ~wen_r) begin // write enable : store data
+		case (lsu_ctrl_r)
+			3'b100: rdata = addr_r==2'b00 ? {24'b0, rdata_word[7:0]} :
+											addr_r==2'b01 ? {24'b0, rdata_word[15:8]} :
+											addr_r==2'b10 ? {24'b0, rdata_word[23:16]} :
+											addr_r==2'b11 ? {24'b0, rdata_word[31:24]} : 32'b0; //lbu 
+			3'b000: rdata = addr_r==2'b00 ? {{24{rdata_word[7]}}, rdata_word[7:0]} :
+											addr_r==2'b01 ? {{24{rdata_word[15]}}, rdata_word[15:8]} :
+											addr_r==2'b10 ? {{24{rdata_word[23]}}, rdata_word[23:16]} :
+											addr_r==2'b11 ? {{24{rdata_word[31]}}, rdata_word[31:24]} : 32'b0; //lb 
+			3'b010: begin //lw
+				rdata = addr_r==2'b00 ? rdata_word : 
+								addr_r==2'b01 ? {rdata_word_n[7:0], rdata_word[31:8]} :
+								addr_r==2'b10 ? {rdata_word_n[15:0], rdata_word[31:16]} :
+								addr_r==2'b11 ? {rdata_word_n[23:0], rdata_word[31:24]} : 32'b0;
+			end
+			3'b101: begin //lhu 
+				rdata = addr_r==2'b00 ? {16'b0, rdata_word[15:0]} :
+								addr_r==2'b01 ? {16'b0, rdata_word[23:8]} :
+								addr_r==2'b10 ? {16'b0, rdata_word[31:16]} :
+								addr_r==2'b11 ? {16'b0, rdata_word_n[7:0], rdata_word[31:24]} : 32'b0;  
+			end
+			3'b001: begin //lh 
+				rdata = addr_r==2'b00 ? {{16{rdata_word[15]}}, rdata_word[15:0]} :
+								addr_r==2'b01 ? {{16{rdata_word[23]}}, rdata_word[23:8]} :
+								addr_r==2'b10 ? {{16{rdata_word[31]}}, rdata_word[31:16]} :
+								addr_r==2'b11 ? {{16{rdata_word_n[7]}},rdata_word_n[7:0], rdata_word[31:24]} : 32'b0;  
+			end
+			default: $finish;
+		endcase
 	end
 end
 
