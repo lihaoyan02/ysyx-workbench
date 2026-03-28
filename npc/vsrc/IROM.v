2ce7aaa159022a37ff4e3efbaa2eba4731338063
100018556-Li Haoyan <1620774239@qq.com>
2026-03-28 20:08:36 +0800

    fix(npc): memory with LFSR random test

--- npc/vsrc/IROM.v
+++ npc/vsrc/IROM.v
@@ -1,4 +1,4 @@
-module IROM #(DATA_WIDTH = 32, ADDR_WIDTH=32, SHIFT_LEN=2) (
+module IROM #(DATA_WIDTH = 32, ADDR_WIDTH=32) (
 	input clk,
     input rst,
     input reqValid,
@@ -14,26 +14,63 @@ import "DPI-C" function int pmem_read(int raddr);
 //   respValid <= reqValid;
 // end
 
+reg state, next_state;
+localparam IDLE=1'b0, WAIT=1'b1;
+
 always @(posedge clk) begin
-    if (reqValid)
-        rdata <= pmem_read(addr);
-    // else if(respValid)
-    //     rdata <= 0;
+    if (rst)
+        state <= IDLE;
+    else
+        state <= next_state;
+end
+
+always @(*) begin
+    case (state)
+        IDLE: next_state = reqValid ? WAIT : IDLE;
+        WAIT: next_state = respValid ? IDLE : WAIT;
+    endcase
+end
+// save mem access info
+
+reg [2:0] cnt;
+reg [3:0] lfsr;
+wire [2:0] rand_val;
+assign rand_val = lfsr[2:0];
+reg [ADDR_WIDTH-1:0] saved_addr;
+always @(posedge clk) begin
+    if (rst)
+        lfsr <= 4'b10;
+    else if (state==IDLE & reqValid) begin
+        lfsr <= {lfsr[0] ^ lfsr[2],lfsr[3:1]};
+    end
 end
 
 always @(posedge clk) begin
-    respValid <= shift_reg[0];
+    if (state==IDLE & reqValid) begin
+        cnt <= rand_val==0 ? 0 : rand_val - 1;
+    end
+    else if(cnt != 0)
+        cnt <= cnt - 1;
 end
-reg [SHIFT_LEN-1:0] shift_reg;
+
 always @(posedge clk) begin
-    if (rst) begin
-        shift_reg <= 0;
+    rdata <= 32'b0;
+    respValid <= 0;
+    if (state==IDLE & reqValid & rand_val==0) begin // cnt==0 direct out
+        rdata <= pmem_read(addr);
+        respValid <= 1;
     end
-    else if (reqValid) begin
-        shift_reg <= {reqValid,shift_reg[SHIFT_LEN-1:1]};
+    else if (state==IDLE & reqValid & rand_val !=0) begin // save mem access info and init cnt
+        cnt <= rand_val - 1;
+        saved_addr <= addr;      
     end
-    else
-        shift_reg <= {1'b0,shift_reg[SHIFT_LEN-1:1]};
+    else if (state==WAIT & cnt == 0 & ~respValid) begin //cnt == 0    
+        rdata <= pmem_read(saved_addr);
+        respValid <= 1;
+    end
+    else if (state==WAIT & cnt!=0) begin
+        cnt <= cnt -1;
+    end
 end
 
 endmodule
