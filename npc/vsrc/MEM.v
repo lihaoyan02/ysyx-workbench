bc798e1b7dd90061daab50c8fd4b9f0d6379c492
tracer-ysyx <tracer@ysyx.org>
2026-03-29 14:10:09 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  14:10:09 up  3:13,  3 users,  load average: 0.66, 0.50, 0.42

--- npc/vsrc/MEM.v
+++ npc/vsrc/MEM.v
@@ -18,6 +18,7 @@ wire req_handshaked, resp_handshaked;
 assign req_handshaked = reqValid & reqReady;
 assign resp_handshaked = respValid & respReady;
 
+`ifndef MEM_MUTI_CYCLE
 /*--------sigle cycle----------*/
 always @(*) begin
     reqReady = reqValid;
@@ -42,102 +43,104 @@ always @(posedge clk) begin
     end
 end
 
+`else
 /*----------multi cycle---------*/
-// reg state, next_state;
-// localparam IDLE=1'b0, WAIT=1'b1;
+reg state, next_state;
+localparam IDLE=1'b0, WAIT=1'b1;
 
-// always @(posedge clk) begin
-//     if (rst)
-//         state <= IDLE;
-//     else
-//         state <= next_state;
-// end
+always @(posedge clk) begin
+    if (rst)
+        state <= IDLE;
+    else
+        state <= next_state;
+end
 
-// always @(*) begin
-//     case (state)
-//         IDLE: next_state = req_handshaked ? WAIT : IDLE;
-//         WAIT: next_state = resp_handshaked ? IDLE : WAIT;
-//     endcase
-// end
-// // save mem access info
+always @(*) begin
+    case (state)
+        IDLE: next_state = req_handshaked ? WAIT : IDLE;
+        WAIT: next_state = resp_handshaked ? IDLE : WAIT;
+    endcase
+end
+// save mem access info
 
-// reg [2:0] cnt;
+reg [2:0] cnt;
 
-// wire [2:0] rand_val;
-// assign rand_val = lfsr[2:0];
-// reg saved_wen;
-// reg [ADDR_WIDTH-1:0] saved_addr;
-// reg [DATA_WIDTH-1:0] saved_wdata;
-// reg [3:0] saved_wmask;
+wire [2:0] rand_val;
+assign rand_val = lfsr[2:0];
+reg saved_wen;
+reg [ADDR_WIDTH-1:0] saved_addr;
+reg [DATA_WIDTH-1:0] saved_wdata;
+reg [3:0] saved_wmask;
 
-// always @(*) begin
-//     if (reqValid & lfsr_rdy[0])
-//         reqReady = 1;
-//     else
-//         reqReady = 0;
-// end
+always @(*) begin
+    if (reqValid & lfsr_rdy[0])
+        reqReady = 1;
+    else
+        reqReady = 0;
+end
 
-// reg [3:0] lfsr_rdy;
-// always @(posedge clk) begin
-//     if (rst)
-//         lfsr_rdy <= 4'b1;
-//     else if (state==IDLE & reqValid) begin
-//         lfsr_rdy <= {lfsr_rdy[0] ^ lfsr_rdy[2],lfsr_rdy[3:1]};
-//     end
-// end
+reg [3:0] lfsr_rdy;
+always @(posedge clk) begin
+    if (rst)
+        lfsr_rdy <= 4'b1;
+    else if (state==IDLE & reqValid) begin
+        lfsr_rdy <= {lfsr_rdy[0] ^ lfsr_rdy[2],lfsr_rdy[3:1]};
+    end
+end
 
-// reg [3:0] lfsr;
-// always @(posedge clk) begin
-//     if (rst)
-//         lfsr <= 4'b1;
-//     else if (state==IDLE & req_handshaked) begin
-//         lfsr <= {lfsr[0] ^ lfsr[2],lfsr[3:1]};
-//     end
-// end
+reg [3:0] lfsr;
+always @(posedge clk) begin
+    if (rst)
+        lfsr <= 4'b1;
+    else if (state==IDLE & req_handshaked) begin
+        lfsr <= {lfsr[0] ^ lfsr[2],lfsr[3:1]};
+    end
+end
 
-// always @(posedge clk) begin
-//     if (state==IDLE & req_handshaked) begin
-//         cnt <= rand_val==0 ? 0 : rand_val - 1;
-//     end
-//     else if(cnt != 0 & state==WAIT)
-//         cnt <= cnt - 1;
-// end
+always @(posedge clk) begin
+    if (state==IDLE & req_handshaked) begin
+        cnt <= rand_val==0 ? 0 : rand_val - 1;
+    end
+    else if(cnt != 0 & state==WAIT)
+        cnt <= cnt - 1;
+end
 
-// always @(posedge clk) begin
-//     if (rst) begin
-//         rdata <= 32'b0;
-//         respValid <= 0; 
-//     end
-//     else if (state==IDLE & req_handshaked & rand_val==0) begin // cnt==0 direct out
-//         if (wen)
-//             pmem_write(addr, wdata, {4'b0,wmask});    
-//         else
-//             rdata <= pmem_read(addr);
-//         respValid <= 1;
-//     end
-//     else if (state==IDLE & req_handshaked & rand_val !=0) begin // save mem access info and init cnt
-//         cnt <= rand_val - 1;
-//         saved_wen <= wen;
-//         saved_addr <= addr;
-//         saved_wdata <= wdata;
-//         saved_wmask <= wmask;  
-//         respValid <= 0;      
-//     end
-//     else if (state==WAIT & cnt == 0 & ~respValid) begin //cnt == 0
-//         if (saved_wen) begin
-//             pmem_write(saved_addr, saved_wdata, {4'b0,saved_wmask});
-//         end     
-//         else
-//             rdata <= pmem_read(saved_addr);
-//         respValid <= 1;
-//     end
-//     else if (state==WAIT & cnt!=0) begin
-//         cnt <= cnt -1;
-//     end
-//     else if (resp_handshaked) begin
-//         rdata <= 0;
-//         respValid <= 0;
-//     end
-// end
+always @(posedge clk) begin
+    if (rst) begin
+        rdata <= 32'b0;
+        respValid <= 0; 
+    end
+    else if (state==IDLE & req_handshaked & rand_val==0) begin // cnt==0 direct out
+        if (wen)
+            pmem_write(addr, wdata, {4'b0,wmask});    
+        else
+            rdata <= pmem_read(addr);
+        respValid <= 1;
+    end
+    else if (state==IDLE & req_handshaked & rand_val !=0) begin // save mem access info and init cnt
+        cnt <= rand_val - 1;
+        saved_wen <= wen;
+        saved_addr <= addr;
+        saved_wdata <= wdata;
+        saved_wmask <= wmask;  
+        respValid <= 0;      
+    end
+    else if (state==WAIT & cnt == 0 & ~respValid) begin //cnt == 0
+        if (saved_wen) begin
+            pmem_write(saved_addr, saved_wdata, {4'b0,saved_wmask});
+        end     
+        else
+            rdata <= pmem_read(saved_addr);
+        respValid <= 1;
+    end
+    else if (state==WAIT & cnt!=0) begin
+        cnt <= cnt -1;
+    end
+    else if (resp_handshaked) begin
+        rdata <= 0;
+        respValid <= 0;
+    end
+end
+`endif
 
 endmodule
