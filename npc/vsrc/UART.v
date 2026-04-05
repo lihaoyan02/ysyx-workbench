d62c0c266b896ef77dd04e5aa31ac6b816784180
tracer-ysyx <tracer@ysyx.org>
2026-04-05 17:08:10 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:08:10 up  7:07,  4 users,  load average: 0.40, 0.39, 0.33

---
+++ npc/vsrc/UART.v
@@ -0,0 +1,299 @@
+//`define MEM_MUTI_CYCLE
+
+module UART #(DATA_WIDTH = 32, ADDR_WIDTH=32, SHIFT_LEN=4, UART_REG_ADDR=32'h1000_0000) (
+	input clk,
+    input rst,
+
+    input AWVALID,
+	output reg AWREADY,
+	input [ADDR_WIDTH-1:0] AWADDR,
+
+	input WVALID,
+	output reg WREADY,
+	input [DATA_WIDTH-1:0] WDATA,
+	input [3:0] WSTRB,
+
+	output reg BVALID,
+	input BREADY,
+	output [1:0] BRESP,
+
+	input ARVALID,
+	output ARREADY,
+	input [ADDR_WIDTH-1:0] ARADDR,
+
+	output reg RVALID,
+	input RREADY,
+	output reg [DATA_WIDTH-1:0] RDATA,
+	output [1:0] RRESP
+);
+
+import "DPI-C" function int pmem_read(int raddr);
+import "DPI-C" function void pmem_write(int waddr, int wdata, byte wmask);
+
+assign BRESP = 0;
+assign RRESP = 0;
+wire AW_handshaked, W_handshaked, AR_handshaked, R_handshaked, B_handshaked;
+assign AW_handshaked = AWVALID & AWREADY;
+assign W_handshaked = WVALID & WREADY;
+assign AR_handshaked = ARVALID & ARREADY;
+assign R_handshaked = RVALID & RREADY;
+assign B_handshaked = BVALID & BREADY;
+
+/*--------Write state machine---------*/
+localparam WIDLE = 2'b0, ASHAK=2'b01, DSHAK=2'b10, WWAIT = 2'b11;
+reg [1:0] wstate;
+reg [1:0] next_wstate;
+always @(posedge clk) begin
+	if (rst)
+		wstate <= WIDLE;
+	else
+		wstate <= next_wstate;
+end
+always @(*) begin
+	case (wstate)
+		WIDLE: begin
+			next_wstate =  AW_handshaked ? (W_handshaked ? WWAIT : ASHAK) : (W_handshaked ? DSHAK : WIDLE);
+		end
+		ASHAK: next_wstate = W_handshaked ? WWAIT : ASHAK;
+		DSHAK: next_wstate = AW_handshaked ? WWAIT : DSHAK;
+		WWAIT: begin
+			next_wstate = B_handshaked ? WIDLE : WWAIT;
+		end
+	endcase
+end
+/*--------Read state machine---------*/
+localparam IDLE = 1'b0, WAIT = 1'b1;
+reg rstate;
+reg next_rstate;
+always @(posedge clk) begin
+	if (rst)
+		rstate <= IDLE;
+	else
+		rstate <= next_rstate;
+end
+always @(*) begin
+	case (rstate)
+		IDLE: begin
+			next_rstate =  AR_handshaked ? WAIT : IDLE;
+		end
+		WAIT: begin
+			next_rstate = R_handshaked ? IDLE : WAIT;
+		end
+	endcase
+end
+
+/*----------------------------Write contrl---------------------------*/
+/*-------------------------------------------------------------------*/
+wire [2:0] w_rand_val;
+`ifndef MEM_MUTI_CYCLE
+/*--------sigle cycle----------*/
+always @(*) begin
+    if (AWVALID & (wstate==WIDLE | wstate==DSHAK))
+        AWREADY = 1;
+    else
+        AWREADY = 0;
+end
+always @(*) begin
+    if (WVALID & (wstate==WIDLE | wstate==ASHAK))
+        WREADY = 1;
+    else
+        WREADY = 0;
+end
+assign w_rand_val = 0;
+
+`else
+/*----------multi cycle---------*/
+reg [3:0] lfsr_awrdy;
+always @(*) begin
+    if (AWVALID & lfsr_awrdy[0] & (wstate==WIDLE | wstate==DSHAK))
+        AWREADY = 1;
+    else
+        AWREADY = 0;
+end
+always @(posedge clk) begin // random time for awready
+    if (rst)
+        lfsr_awrdy <= 4'b1;
+    else if ((wstate==WIDLE | wstate==DSHAK) & AWVALID) begin //wait for aw handshake
+        lfsr_awrdy <= {lfsr_awrdy[0] ^ lfsr_awrdy[2],lfsr_awrdy[3:1]};
+    end
+end
+
+reg [3:0] lfsr_wrdy;
+always @(*) begin
+    if (WVALID & lfsr_wrdy[0] & (wstate==WIDLE | wstate==ASHAK))
+        WREADY = 1;
+    else
+        WREADY = 0;
+end
+always @(posedge clk) begin // random time for wready
+    if (rst)
+        lfsr_wrdy <= 4'b10;
+    else if ((wstate==WIDLE | wstate==ASHAK) & WVALID) begin //wait for w handshake
+        lfsr_wrdy <= {lfsr_wrdy[0] ^ lfsr_wrdy[2],lfsr_wrdy[3:1]};
+    end
+end
+
+/*------------rand val generation------------*/
+reg [3:0] w_lfsr;
+assign w_rand_val = w_lfsr[2:0];
+
+always @(posedge clk) begin
+    if (rst)
+        w_lfsr <= 4'b1;
+    else if (B_handshaked) begin
+        w_lfsr <= {w_lfsr[0] ^ w_lfsr[2],w_lfsr[3:1]};
+    end
+end
+
+`endif
+
+reg [2:0] w_cnt;
+always @(posedge clk) begin
+    if ((wstate==WIDLE | wstate==DSHAK) & AW_handshaked) begin
+        w_cnt <= w_rand_val==0 ? 0 : w_rand_val - 1;
+    end
+    else if(w_cnt != 0 & wstate==WWAIT)
+        w_cnt <= w_cnt - 1;
+end
+// write control
+reg [DATA_WIDTH-1:0] saved_wdata;
+reg [7:0] uart_data_reg;
+reg addr_match;
+always @(posedge clk) begin
+    if (rst) begin
+        BVALID <= 0; 
+        BVALID <= 0;
+        uart_data_reg <= 0;
+        addr_match <= 0;
+    end
+    else if (wstate==WIDLE & AW_handshaked & W_handshaked & w_rand_val==0) begin // cnt==0 direct out
+        if (AWADDR==UART_REG_ADDR) begin
+            uart_data_reg <= WDATA[7:0];
+            $write("%c",WDATA[7:0]);
+        end   
+        BVALID <= 1;
+    end
+    else if (wstate==DSHAK & AW_handshaked & w_rand_val==0) begin // cnt==0 direct out
+        if (AWADDR==UART_REG_ADDR) begin
+            uart_data_reg <= saved_wdata[7:0];
+            $write("%c",saved_wdata[7:0]);
+        end   
+        BVALID <= 1;
+    end
+    else if (wstate==ASHAK & W_handshaked & w_rand_val==0) begin // cnt==0 direct out
+        if (addr_match) begin
+            uart_data_reg <= WDATA[7:0];
+            $write("%c",WDATA[7:0]);
+        end
+        BVALID <= 1;
+    end
+    else if (wstate==WWAIT & w_cnt == 0 & ~BVALID) begin //cnt == 0
+        if (addr_match) begin
+            uart_data_reg <= saved_wdata[7:0];
+            $write("%c",saved_wdata[7:0]);
+        end 
+        BVALID <= 1;
+    end
+    else if (AW_handshaked & W_handshaked) begin // save mem access info and init cnt
+        addr_match <= AWADDR==UART_REG_ADDR;
+        saved_wdata <= WDATA;
+        BVALID <= 0;      
+    end
+    else if (AW_handshaked) begin // save mem access info and init cnt
+        addr_match <= AWADDR==UART_REG_ADDR;
+        BVALID <= 0;      
+    end
+    else if (W_handshaked) begin // save mem access info and init cnt
+        saved_wdata <= WDATA;
+        BVALID <= 0;      
+    end
+    else if (B_handshaked) begin
+        BVALID <= 0;
+        addr_match <= 0;
+    end
+end
+
+/*--------------------------Read contrl------------------------------*/
+/*-------------------------------------------------------------------*/
+wire [2:0] r_rand_val;
+`ifndef MEM_MUTI_CYCLE
+/*--------sigle cycle----------*/
+always @(*) begin
+    if (ARVALID & rstate==IDLE)
+        ARREADY = 1;
+    else
+        ARREADY = 0;
+end
+assign r_rand_val = 0;
+
+`else
+/*----------multi cycle---------*/
+always @(*) begin
+    if (ARVALID & lfsr_rdy[0] & rstate==IDLE)
+        ARREADY = 1;
+    else
+        ARREADY = 0;
+end
+reg [3:0] lfsr_rdy;
+always @(posedge clk) begin // random time for awready
+    if (rst)
+        lfsr_rdy <= 4'b1;
+    else if (rstate==IDLE & ARVALID) begin //wait for ar handshake
+        lfsr_rdy <= {lfsr_rdy[0] ^ lfsr_rdy[2],lfsr_rdy[3:1]};
+    end
+end
+
+/*------------read rand val generation------------*/
+reg [3:0] r_lfsr;
+assign r_rand_val = r_lfsr[2:0];
+always @(posedge clk) begin
+    if (rst)
+        r_lfsr <= 4'b1;
+    else if (R_handshaked) begin
+        r_lfsr <= {r_lfsr[0] ^ r_lfsr[2],r_lfsr[3:1]};
+    end
+end
+
+`endif
+
+reg [2:0] r_cnt;
+always @(posedge clk) begin
+    if (rstate==IDLE & AR_handshaked) begin
+        r_cnt <= r_rand_val==0 ? 0 : r_rand_val - 1;
+    end
+    else if(r_cnt != 0 & rstate==WAIT)
+        r_cnt <= r_cnt - 1;
+end
+
+reg addr_rmatch;
+always @(posedge clk) begin
+    if (rst) begin
+        RDATA <= 32'b0;
+        RVALID <= 0;
+        addr_rmatch <= 0;
+    end
+    else if (rstate==IDLE & AR_handshaked & r_rand_val==0) begin // cnt==0 direct out
+        if (ARADDR==UART_REG_ADDR) begin
+            RDATA <= {24'b0,uart_data_reg};
+        end
+        RVALID <= 1;
+    end
+    else if (rstate==WAIT & r_cnt == 0 & ~RVALID) begin //cnt == 0
+        if (addr_rmatch) begin
+            RDATA <= {24'b0,uart_data_reg};
+        end
+        RVALID <= 1;
+    end
+    else if (AR_handshaked) begin // save mem access info and init cnt
+        addr_rmatch <= ARADDR==UART_REG_ADDR;
+        RVALID <= 0;      
+    end
+    else if (R_handshaked) begin
+        RVALID <= 0;
+        addr_rmatch <= 0;
+    end
+end
+
+
+
+endmodule
--- npc/vsrc/arbiter.v
+++ npc/vsrc/arbiter.v
@@ -66,87 +66,187 @@ module arbiter #(DATA_WIDTH = 32, ADDR_WIDTH=32) (
 	input mem_RVALID,
 	output mem_RREADY,
 	input [ADDR_WIDTH-1:0] mem_RDATA,
-	input [1:0] mem_RRESP
+	input [1:0] mem_RRESP,
+//uart
+    output uart_AWVALID,
+	input uart_AWREADY,
+	output [ADDR_WIDTH-1:0] uart_AWADDR,
+
+	output uart_WVALID,
+	input uart_WREADY,
+	output [ADDR_WIDTH-1:0] uart_WDATA,
+	output [3:0] uart_WSTRB,
+
+	input uart_BVALID,
+	output uart_BREADY,
+	input [1:0] uart_BRESP,
+
+	output uart_ARVALID,
+	input uart_ARREADY,
+	output [ADDR_WIDTH-1:0] uart_ARADDR,
+
+	input uart_RVALID,
+	output uart_RREADY,
+	input [ADDR_WIDTH-1:0] uart_RDATA,
+	input [1:0] uart_RRESP
 );
-
+localparam UART_REG_ADDR=32'h1000_0000;
 localparam IDLE=2'b00, GRANT_IFU=2'b01, GRANT_LSU=2'b10;
-reg [1:0] state, next_state;
+localparam GRANT_UART=2'b01, GRANT_MEM=2'b10;
+reg [1:0] mstate, next_mstate;
+reg [1:0] sstate, next_sstate;
 
-wire ifu_req = ifu_AWVALID | ifu_WVALID | ifu_ARVALID;
-wire lsu_req = lsu_AWVALID | lsu_WVALID | lsu_ARVALID;
+wire ifu_req = ifu_AWVALID| ifu_ARVALID;
+wire lsu_req = lsu_AWVALID| lsu_ARVALID;
 
 // when IDLE default grant ifu(minimise latency)
-// wire grant_lsu = ((state==IDLE) & lsu_req) | state==GRANT_LSU; //combi loop
-wire grant_lsu = state==GRANT_LSU;
-
+// wire grant_lsu = ((mstate==IDLE) & lsu_req) | mstate==GRANT_LSU; //combi loop
+wire grant_lsu = mstate==GRANT_LSU;
+wire grant_ifu = mstate==GRANT_IFU;
+wire grant_uart = sstate==GRANT_UART;
+wire grant_mem = sstate==GRANT_MEM;
 always @(posedge clk) begin
-    if (rst)
-        state <= IDLE;
-    else
-        state <= next_state;
+    if (rst) begin
+        mstate <= IDLE;
+		sstate <= IDLE;
+	end
+    else begin
+        mstate <= next_mstate;
+		sstate <= next_sstate;
+	end
 end
 
 always @(*) begin
-    case (state)
+    case (mstate)
         IDLE: begin
             if (ifu_req) begin
-                next_state = GRANT_IFU;
+                next_mstate = GRANT_IFU;
+				if (ifu_AWVALID)
+					next_sstate = ifu_AWADDR[31:12]==20'h1000_0 ? GRANT_UART : GRANT_MEM;
+				else
+					next_sstate = ifu_ARADDR[31:12]==20'h1000_0 ? GRANT_UART : GRANT_MEM;
             end
             else if (lsu_req) begin
-                next_state = GRANT_LSU;
+                next_mstate = GRANT_LSU;
+				if (lsu_AWVALID)
+					next_sstate = lsu_AWADDR[31:12]==20'h1000_0 ? GRANT_UART : GRANT_MEM;
+				else
+					next_sstate = lsu_ARADDR[31:12]==20'h1000_0 ? GRANT_UART : GRANT_MEM;
             end
-            else
-                next_state = IDLE;
+            else begin
+                next_mstate = IDLE;
+				next_sstate = IDLE;
+			end
         end 
         GRANT_IFU: begin
-			if(ifu_RVALID & ifu_RREADY & lsu_req)
-                next_state = GRANT_LSU;
-            else if (ifu_BVALID & ifu_BREADY & lsu_req)
-                next_state = GRANT_LSU;
-            else if(ifu_RVALID & ifu_RREADY)
-                next_state = IDLE;
-            else if (ifu_BVALID & ifu_BREADY)
-                next_state = IDLE;
-            else
-                next_state = GRANT_IFU;
+			if(ifu_RVALID & ifu_RREADY & lsu_req) begin
+                next_mstate = GRANT_LSU;
+				if (lsu_AWVALID)
+					next_sstate = lsu_AWADDR[31:12]==20'h1000_0 ? GRANT_UART : GRANT_MEM;
+				else
+					next_sstate = lsu_ARADDR[31:12]==20'h1000_0 ? GRANT_UART : GRANT_MEM;
+			end
+            else if (ifu_BVALID & ifu_BREADY & lsu_req) begin
+                next_mstate = GRANT_LSU;
+				if (lsu_AWVALID)
+					next_sstate = lsu_AWADDR[31:12]==20'h1000_0 ? GRANT_UART : GRANT_MEM;
+				else
+					next_sstate = lsu_ARADDR[31:12]==20'h1000_0 ? GRANT_UART : GRANT_MEM;
+			end
+            else if(ifu_RVALID & ifu_RREADY) begin
+                next_mstate = IDLE;
+				next_sstate = IDLE;
+			end
+            else if (ifu_BVALID & ifu_BREADY) begin
+                next_mstate = IDLE;
+				next_sstate = IDLE;
+			end
+            else begin
+                next_mstate = GRANT_IFU;
+				next_sstate = sstate==GRANT_UART ? GRANT_UART : GRANT_MEM;
+			end
         end
         GRANT_LSU: begin
-            if(lsu_RVALID & lsu_RREADY)
-                next_state = IDLE;
-            else if (lsu_BVALID & lsu_BREADY)
-                next_state = IDLE;
-            else
-                next_state = GRANT_LSU;
+            if(lsu_RVALID & lsu_RREADY) begin
+                next_mstate = IDLE;
+				next_sstate = IDLE;
+			end
+            else if (lsu_BVALID & lsu_BREADY) begin
+                next_mstate = IDLE;
+				next_sstate = IDLE;
+			end
+            else begin
+                next_mstate = GRANT_LSU;
+				next_sstate = sstate==GRANT_UART ? GRANT_UART : GRANT_MEM;
+			end
         end
-        default: next_state = IDLE;
+        default: begin
+			next_mstate = IDLE;
+			next_sstate = IDLE;
+		end
     endcase
 end
 
-assign mem_AWVALID = grant_lsu ? lsu_AWVALID : ifu_AWVALID;
-assign mem_AWADDR = grant_lsu ? lsu_AWADDR : ifu_AWADDR;
-assign mem_WVALID = grant_lsu ? lsu_WVALID : ifu_WVALID;
-assign mem_WDATA = grant_lsu ? lsu_WDATA : ifu_WDATA;
-assign mem_WSTRB = grant_lsu ? lsu_WSTRB : ifu_WSTRB;
-assign mem_BREADY = grant_lsu ? lsu_BREADY : ifu_BREADY;
-assign mem_ARVALID = grant_lsu ? lsu_ARVALID : ifu_ARVALID;
-assign mem_ARADDR = grant_lsu ? lsu_ARADDR : ifu_ARADDR;
-assign mem_RREADY = grant_lsu ? lsu_RREADY : ifu_RREADY;
+wire inter_AWVALID, inter_AWREADY, inter_WVALID, inter_WREADY, 
+inter_BVALID, inter_BREADY, inter_ARVALID, inter_ARREADY, inter_RVALID,inter_RREADY;
+wire [DATA_WIDTH-1:0] inter_AWADDR, inter_WDATA, inter_ARADDR, inter_RDATA;
+wire [3:0] inter_WSTRB;
+wire [1:0] inter_BRESP, inter_RRESP;
 
-assign ifu_AWREADY = grant_lsu ? 0 : mem_AWREADY;
-assign ifu_WREADY = grant_lsu ? 0 : mem_WREADY;
-assign ifu_BVALID = grant_lsu ? 0 : mem_BVALID;
-assign ifu_BRESP = grant_lsu ? 0 : mem_BRESP;
-assign ifu_ARREADY = grant_lsu ? 0 : mem_ARREADY;
-assign ifu_RVALID = grant_lsu ? 0 : mem_RVALID;
-assign ifu_RDATA = grant_lsu ? 0 : mem_RDATA;
-assign ifu_RRESP = grant_lsu ? 0 : mem_RRESP;
+assign inter_AWVALID = grant_lsu ? lsu_AWVALID : grant_ifu ? ifu_AWVALID : 0;
+assign inter_AWADDR = grant_lsu ? lsu_AWADDR : grant_ifu ? ifu_AWADDR : 0;
+assign inter_WVALID = grant_lsu ? lsu_WVALID : grant_ifu ? ifu_WVALID : 0;
+assign inter_WDATA = grant_lsu ? lsu_WDATA : grant_ifu ? ifu_WDATA : 0;
+assign inter_WSTRB = grant_lsu ? lsu_WSTRB : grant_ifu ? ifu_WSTRB : 0;
+assign inter_BREADY = grant_lsu ? lsu_BREADY : grant_ifu ? ifu_BREADY : 0;
+assign inter_ARVALID = grant_lsu ? lsu_ARVALID : grant_ifu ? ifu_ARVALID : 0;
+assign inter_ARADDR = grant_lsu ? lsu_ARADDR : grant_ifu ? ifu_ARADDR : 0;
+assign inter_RREADY = grant_lsu ? lsu_RREADY : grant_ifu ? ifu_RREADY : 0;
 
-assign lsu_AWREADY = grant_lsu ? mem_AWREADY : 0;
-assign lsu_WREADY = grant_lsu ? mem_WREADY : 0;
-assign lsu_BVALID = grant_lsu ? mem_BVALID : 0;
-assign lsu_BRESP = grant_lsu ? mem_BRESP : 0;
-assign lsu_ARREADY = grant_lsu ? mem_ARREADY : 0;
-assign lsu_RVALID = grant_lsu ? mem_RVALID : 0;
-assign lsu_RDATA = grant_lsu ? mem_RDATA : 0;
-assign lsu_RRESP = grant_lsu ? mem_RRESP : 0;
+assign ifu_AWREADY = grant_lsu ? 0 : inter_AWREADY;
+assign ifu_WREADY = grant_lsu ? 0 : inter_WREADY;
+assign ifu_BVALID = grant_lsu ? 0 : inter_BVALID;
+assign ifu_BRESP = grant_lsu ? 0 : inter_BRESP;
+assign ifu_ARREADY = grant_lsu ? 0 : inter_ARREADY;
+assign ifu_RVALID = grant_lsu ? 0 : inter_RVALID;
+assign ifu_RDATA = grant_lsu ? 0 : inter_RDATA;
+assign ifu_RRESP = grant_lsu ? 0 : inter_RRESP;
+
+assign lsu_AWREADY = grant_lsu ? inter_AWREADY : 0;
+assign lsu_WREADY = grant_lsu ? inter_WREADY : 0;
+assign lsu_BVALID = grant_lsu ? inter_BVALID : 0;
+assign lsu_BRESP = grant_lsu ? inter_BRESP : 0;
+assign lsu_ARREADY = grant_lsu ? inter_ARREADY : 0;
+assign lsu_RVALID = grant_lsu ? inter_RVALID : 0;
+assign lsu_RDATA = grant_lsu ? inter_RDATA : 0;
+assign lsu_RRESP = grant_lsu ? inter_RRESP : 0;
+
+assign inter_AWREADY = grant_uart ? uart_AWREADY : grant_mem ? mem_AWREADY : 0;
+assign inter_WREADY = grant_uart ? uart_WREADY : grant_mem ? mem_WREADY : 0;
+assign inter_BVALID = grant_uart ? uart_BVALID : grant_mem ? mem_BVALID : 0;
+assign inter_BRESP = grant_uart ? uart_BRESP : grant_mem ? mem_BRESP : 0;
+assign inter_ARREADY = grant_uart ? uart_ARREADY : grant_mem ? mem_ARREADY : 0;
+assign inter_RVALID = grant_uart ? uart_RVALID : grant_mem ? mem_RVALID : 0;
+assign inter_RDATA = grant_uart ? uart_RDATA : grant_mem ? mem_RDATA : 0;
+assign inter_RRESP = grant_uart ? uart_RRESP  : grant_mem ? mem_RRESP : 0;
+
+assign uart_AWVALID = grant_uart ? inter_AWVALID : 0;
+assign uart_AWADDR = grant_uart ? inter_AWADDR : 0;
+assign uart_WVALID = grant_uart ? inter_WVALID : 0;
+assign uart_WDATA = grant_uart ? inter_WDATA : 0;
+assign uart_WSTRB = grant_uart ? inter_WSTRB : 0;
+assign uart_BREADY = grant_uart ? inter_BREADY : 0;
+assign uart_ARVALID = grant_uart ? inter_ARVALID : 0;
+assign uart_ARADDR = grant_uart ? inter_ARADDR : 0;
+assign uart_RREADY = grant_uart ? inter_RREADY : 0;
+
+assign mem_AWVALID = grant_mem ? inter_AWVALID : 0;
+assign mem_AWADDR = grant_mem ? inter_AWADDR : 0;
+assign mem_WVALID = grant_mem ? inter_WVALID : 0;
+assign mem_WDATA = grant_mem ? inter_WDATA : 0;
+assign mem_WSTRB = grant_mem ? inter_WSTRB : 0;
+assign mem_BREADY = grant_mem ? inter_BREADY : 0;
+assign mem_ARVALID = grant_mem ? inter_ARVALID : 0;
+assign mem_ARADDR = grant_mem ? inter_ARADDR : 0;
+assign mem_RREADY = grant_mem ? inter_RREADY : 0;
 endmodule
