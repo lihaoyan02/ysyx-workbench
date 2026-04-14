0895cb675c549041177aa70e26442e55a660e633
tracer-ysyx <tracer@ysyx.org>
2026-04-14 11:12:09 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  11:12:09 up  1:53,  2 users,  load average: 0.48, 0.45, 0.32

--- npc/test/char-test.c
+++ npc/test/char-test.c
@@ -1,7 +1,15 @@
 #define UART_BASE 0x10000000L
 #define UART_TX 0
 
+#define SRAM_BASE 0x0f000000
+#define STACK_TOP SRAM_BASE+(1024*1024)
+
+__attribute__((naked))
 void _start() {
+    __asm__ volatile (
+        "li sp, %0\n"
+        ::"i"(STACK_TOP)
+    );
     *(volatile char *)(UART_BASE + UART_TX) = 'A';
     *(volatile char *)(UART_BASE + UART_TX) = '\n';
     while (1);    
