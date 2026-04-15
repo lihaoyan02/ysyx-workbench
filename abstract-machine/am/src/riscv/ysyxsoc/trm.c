3653d58ec9140baca03561605bcaa02ff5379b0f
tracer-ysyx <tracer@ysyx.org>
2026-04-15 14:46:31 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  14:46:31 up  2:01,  3 users,  load average: 0.00, 0.09, 0.13

---
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -0,0 +1,38 @@
+#include <am.h>
+#include <klib-macros.h>
+#include <riscv/riscv.h>
+
+extern char _heap_start;
+extern char _heap_end;
+int main(const char *args);
+
+// extern char _pmem_start;
+// #define PMEM_SIZE (128 * 1024 * 1024)
+// #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
+
+Area heap = RANGE(&_heap_start, &_heap_end);
+static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
+
+void putch(char ch) {
+	 outb(0x10000000, ch);	
+	 //outb(0xa00003f8, ch);
+}
+
+void halt(int code) {
+	asm volatile("mv a0, %0; ebreak" : :"r"(code));
+	while(1);
+}
+
+//#include <stdio.h>
+void _trm_init() {
+	/*
+	uint32_t marchid;
+	uint32_t mvendorid;
+	asm volatile("csrr %0, marchid":"=r" (marchid));
+	asm volatile("csrr %0, mvendorid":"=r" (mvendorid));
+	char *arch_str = (char*)&mvendorid;
+	printf("%c%c%c%c-%d\n",arch_str[3],arch_str[2],arch_str[1],arch_str[0],marchid);
+	*/
+  int ret = main(mainargs);
+  halt(ret);
+}
