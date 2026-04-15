64a810fdafaa7a00a6ad730614597a677cf19b2b
tracer-ysyx <tracer@ysyx.org>
2026-04-15 17:11:01 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:11:01 up  4:25,  4 users,  load average: 0.35, 0.23, 0.25

--- npc/csrc/memory.cpp
+++ npc/csrc/memory.cpp
@@ -72,8 +72,8 @@ extern "C" void pmem_write(int waddr, int wdata, char wmask) {
 
 static const uint32_t default_img[] = {
 	0x00000297,  // auipc t0,0
-	0x00028823,  // sb  zero,16(t0)
-	0x0102c503,  // lbu a0,16(t0)
+	// 0x00028823,  // sb  zero,16(t0)
+	// 0x0102c503,  // lbu a0,16(t0)
 	0x00100073, // ebreak 
 	0xdeadbeef, // some data
 	//0x01400513, 0x010000e7, 0x00c000e7, 0x00100073,
