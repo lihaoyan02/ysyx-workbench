9c00c6721b737900ba00eea0d27b69e064f10ea4
tracer-ysyx <tracer@ysyx.org>
2026-04-18 15:17:16 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:17:16 up  1:21,  4 users,  load average: 0.32, 0.20, 0.18

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -27,7 +27,7 @@ static void init_uart() {
 }
 void putch(char ch) {
 	// wait for Transmitter Holding Register (THR) empty
-	while ((inb(UART_LSR) & 0x20)==0) 
+	// while ((inb(UART_LSR) & 0x20)==0) 
 		;
 	outb(UART_BASE, ch);
 }
