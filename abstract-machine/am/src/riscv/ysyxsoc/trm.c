1edf5d52efc31a2b1a2838d5e59e23ff40c1b340
tracer-ysyx <tracer@ysyx.org>
2026-04-18 15:17:53 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:17:53 up  1:22,  4 users,  load average: 0.64, 0.32, 0.21

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -15,16 +15,16 @@ int main(const char *args);
 Area heap = RANGE(&_heap_start, &_heap_end);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
-static void init_uart() {
-	outb(UART_IER, 0x00); // Disable all interrupts
-	// 115200 bps, 8N1
-	outb(UART_LCR, 0x83u); // Divisor Latch Access Bit (DLAB) set
-	outb(UART_BASE + 0x00, 0x01); // Set divisor to 1 (LSB) 115200 bps
-	outb(UART_BASE + 0x01, 0x00); //                  (MSB)
+// static void init_uart() {
+// 	outb(UART_IER, 0x00); // Disable all interrupts
+// 	// 115200 bps, 8N1
+// 	outb(UART_LCR, 0x83u); // Divisor Latch Access Bit (DLAB) set
+// 	outb(UART_BASE + 0x00, 0x01); // Set divisor to 1 (LSB) 115200 bps
+// 	outb(UART_BASE + 0x01, 0x00); //                  (MSB)
 
-	outb(UART_LCR, 0x03); // 8 bits, no parity, one stop bit
-	outb(UART_FCR, 0x07); // Enable FIFO, clear RX/TX FIFO
-}
+// 	outb(UART_LCR, 0x03); // 8 bits, no parity, one stop bit
+// 	outb(UART_FCR, 0x07); // Enable FIFO, clear RX/TX FIFO
+// }
 void putch(char ch) {
 	// wait for Transmitter Holding Register (THR) empty
 	// while ((inb(UART_LSR) & 0x20)==0) 
@@ -47,7 +47,7 @@ void _trm_init() {
 	char *arch_str = (char*)&mvendorid;
 	printf("%c%c%c%c-%d\n",arch_str[3],arch_str[2],arch_str[1],arch_str[0],marchid);
 	*/
-	init_uart();
+	// init_uart();
   	int ret = main(mainargs);
   	halt(ret);
 }
