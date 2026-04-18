6b78a5c12a2129e89f08ade63b508c30f395753d
tracer-ysyx <tracer@ysyx.org>
2026-04-18 14:20:06 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-107-generic #107~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 18 23:40:43 UTC  x86_64 x86_64 x86_64 GNU/Linux  14:20:06 up 24 min,  3 users,  load average: 0.27, 0.21, 0.20

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -6,16 +6,25 @@ extern char _heap_start;
 extern char _heap_end;
 int main(const char *args);
 
-// extern char _pmem_start;
-// #define PMEM_SIZE (128 * 1024 * 1024)
-// #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
+#define UART_BASE 0x10000000
+#define UART_LCR (UART_BASE + 0x03) // Line Control Register
+#define UART_LSR (UART_BASE + 0x05) // Line Status Register
 
 Area heap = RANGE(&_heap_start, &_heap_end);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
+void init_uart() {
+	// 115200 bps, 8N1
+	outb(UART_LCR, 0x83); // Divisor Latch Access Bit (DLAB) set
+	outb(UART_BASE + 0x00, 0x01); // Set divisor to 1 (LSB) 115200 bps
+	outb(UART_BASE + 0x01, 0x00); //                  (MSB)
+
+	outb(UART_LCR, 0x03); // 8 bits, no parity, one stop bit
+}
 void putch(char ch) {
-	 outb(0x10000000, ch);	
-	 //outb(0xa00003f8, ch);
+	// wait for Transmitter Holding Register (THR) empty
+	while ((inb(UART_LSR) & 0x20)==0) ;
+	outb(UART_BASE, ch);
 }
 
 void halt(int code) {
@@ -33,6 +42,7 @@ void _trm_init() {
 	char *arch_str = (char*)&mvendorid;
 	printf("%c%c%c%c-%d\n",arch_str[3],arch_str[2],arch_str[1],arch_str[0],marchid);
 	*/
-  int ret = main(mainargs);
-  halt(ret);
+	init_uart();
+  	int ret = main(mainargs);
+  	halt(ret);
 }
