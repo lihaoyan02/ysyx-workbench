b5cfbfa04961ee79bd73d8318d160ef39bd8f039
tracer-ysyx <tracer@ysyx.org>
2026-05-18 00:16:53 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  00:16:53 up 13:18,  3 users,  load average: 0.65, 0.26, 0.19

--- abstract-machine/am/src/riscv/ysyxsoc/cte.c
+++ abstract-machine/am/src/riscv/ysyxsoc/cte.c
@@ -7,6 +7,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
+    printf("[EV_YIELD] from mepc=%x\n", c->mepc);
     switch (c->mcause) {
 			case 0xb: ev.event = EVENT_YIELD; c->mepc += 4; break;
       default: ev.event = EVENT_ERROR; break;
