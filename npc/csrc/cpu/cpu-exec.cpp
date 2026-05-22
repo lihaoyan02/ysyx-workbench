c7d39f0dbd6a0c31ea5862d3a677ba1c365f06f2
tracer-ysyx <tracer@ysyx.org>
2026-05-22 20:37:08 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  20:37:08 up  7:29,  4 users,  load average: 0.97, 0.63, 0.47

--- npc/csrc/cpu/cpu-exec.cpp
+++ npc/csrc/cpu/cpu-exec.cpp
@@ -134,7 +134,7 @@ extern "C" void npctrap(int a0, int pc) {
 }
 
 static void exec_one_inst() {
-	for(int i =0; i<3000; i++) {
+	for(int i =0; i<4000; i++) {
 		single_cycle();
 		uint32_t current_state = core_read_state();
 		if(current_state==1) {
