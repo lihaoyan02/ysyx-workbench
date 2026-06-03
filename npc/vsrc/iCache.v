2bf6e5996728ef7d5b259e92d0568e6e43b7ba04
tracer-ysyx <tracer@ysyx.org>
2026-06-03 16:17:06 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  16:17:06 up  5:54,  4 users,  load average: 0.31, 0.24, 0.37

--- npc/vsrc/iCache.v
+++ npc/vsrc/iCache.v
@@ -143,17 +143,17 @@ always @(posedge clk) begin
                     end
                     else begin
                         cnt <= cnt + 1;
-                        if (((raddr>=SDRAM_ADDR_DOWN) && (raddr<SDRAM_ADDR_UP))) begin
-                            state <= FETCH_BURST;
-                            ARLEN <= BLOCK_SIZE/4-1;
-                            ARBURST <= 2'b01;
-                        end
-                        else begin
+                        // if (((raddr>=SDRAM_ADDR_DOWN) && (raddr<SDRAM_ADDR_UP))) begin
+                        //     state <= FETCH_BURST;
+                        //     ARLEN <= BLOCK_SIZE/4-1;
+                        //     ARBURST <= 2'b01;
+                        // end
+                        // else begin
                             state <= FETCH_SIGLE;
                             ARLEN <= 0;
                             ARBURST <= 2'b0;
                             ptr <= 0;
-                        end
+                        // end
                         araddr_r <= raddr;
                         ARVALID <= 1;
                         ARADDR <= {raddr[XLEN-1:OFFSET_BIT_H],{OFFSET_LEN{1'b0}}};
