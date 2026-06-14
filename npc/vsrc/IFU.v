4381815b02b2ab869eeb34aa188fe69537aa0332
100018556-Li Haoyan <1620774239@qq.com>
2026-06-14 11:28:26 +0800

    pipeline IFU

--- npc/vsrc/IFU.v
+++ npc/vsrc/IFU.v
@@ -1,13 +1,17 @@
 module IFU #(INST_WIDTH = 32, ADDR_WIDTH = 32)(
 	input clk,
 	input rst,
-	input exu_j_pc,
-	input [ADDR_WIDTH-1:0] j_pc_addr,
-	input ready_npc_in,
-	output reg [ADDR_WIDTH-1:0] pc,
-	output reg [INST_WIDTH-1:0] inst_fetch,
-	output reg inst_valid,
-	output wb_valid,
+
+	input ex_if_jvalid,
+	output if_ex_jready,
+	input [ADDR_WIDTH-1:0] ex_if_jpc,
+
+	// input ready_npc_in,
+	output if_id_valid,
+	input id_if_ready,
+	output [ADDR_WIDTH-1:0] if_id_pc,
+	output [INST_WIDTH-1:0] if_id_inst,
+	// output wb_valid,
 	
 	// to icache
 	output [ADDR_WIDTH-1:0] raddr,
@@ -24,65 +28,40 @@ import "DPI-C" function void performance_counter(int category);
 wire AR_handshaked, R_handshaked;
 assign AR_handshaked = avalid & aready;
 assign R_handshaked = rvalid & rready;
-assign rready = rvalid & state==WAIT;
 
-localparam IDLE = 1'b0, WAIT = 1'b1;
-reg state, next_state;
+/*---------------------if_id-----------------------*/
+reg inst_valid;
 always @(posedge clk) begin
-	if (rst)
-		state <= IDLE;
-	else
-		state <= next_state;
-end
-
-
-reg R_handshaked_r;
-always @(*) begin
-	case (state)
-		IDLE:
-			next_state = AR_handshaked ? WAIT : IDLE;
-		WAIT:
-			next_state = (ready_npc_in & R_handshaked_r) ? IDLE : WAIT; //(ready_npc_in & R_handshaked) |
-	endcase
-end
-
-always @(posedge clk) begin
-	if (state==WAIT & R_handshaked) begin
-		R_handshaked_r <= 1;
-		performance_counter(0);
+	if (rst) begin
+		inst_valid <= 0;
 	end
-	else if (state==IDLE) begin
-		R_handshaked_r <= 0;
+	else if (R_handshaked & ~ex_if_jvalid) begin
+		inst_valid <= 1;
+	end
+	else if (if_id_valid & id_if_ready) begin
+		inst_valid <= 0;
 	end
 end
 
-// reg if_id_valid;
-// always @(posedge clk) begin
-// 	if (rst) begin
-// 		if_id_valid <= 0;
-// 	end
-// 	else if (R_handshaked) begin
-// 		if_id_valid <= 0;
-// 	end
-// 	else if (if_id_valid & id_if_ready) begin
-// 		if_id_valid <= 0;
-// 	end
-// 	else if (flush) begin
-// 		if_id_valid <= 0;
-// 	end
-// 	else begin
-// 		if_id_valid <= if_id_valid;
-// 	end
-// end
+reg [INST_WIDTH-1:0] inst_fetch;
+always @(posedge clk) begin
+	if (rst) begin
+		inst_fetch <= 0;
+	end
+	else if (R_handshaked) begin
+		inst_fetch <= rdata;
+	end
+end
 
-assign avalid = ~rst & state==IDLE;
-assign raddr = pc;
-assign inst_fetch = R_handshaked ? rdata : 0;
-assign inst_valid = R_handshaked;
-
+assign if_id_valid = inst_valid;
+assign if_id_inst = inst_fetch;
+assign if_id_pc = pc;
+/*--------------ICache-------------------------*/
+// change pc at R_handshaked
 wire [ADDR_WIDTH-1:0] next_pc;
-assign next_pc = exu_j_pc ? j_pc_addr : pc + 4; 
-assign wb_valid = state==WAIT & next_state==IDLE;
+assign next_pc = pc + 4; 
+reg [ADDR_WIDTH-1:0] pc;
+// assign wb_valid = state==WAIT & next_state==IDLE;
 always @(posedge clk) begin
 	`ifndef CONFIG_TARGET_SOC
 	if (rst) pc <= 32'h8000_0000;//{ADDR_WIDTH{1'b0}}; 
@@ -90,22 +69,34 @@ always @(posedge clk) begin
 	// if (rst) pc <= 32'h2000_0000; // MROM
 	if (rst) pc <= 32'h3000_0000; // flash
 	`endif
-	else if(wb_valid)
+	else if (R_handshaked & ex_if_jvalid) begin
+		pc <= ex_if_jpc;
+	end
+	else if(R_handshaked)
 		pc <= next_pc;
 end
 
-reg [INST_WIDTH-1:0] inst_fetch_r;
+reg if_ica_avalid;
 always @(posedge clk) begin
 	if (rst) begin
-		inst_fetch_r <= 0;
+		if_ica_avalid <= 1; // fetch first inst
 	end
-	else if (R_handshaked) begin
-		inst_fetch_r <= inst_fetch;
+	else if (AR_handshaked) begin
+		if_ica_avalid <= 0;
+	end
+	else if (R_handshaked) begin // launch next fetch
+		if_ica_avalid <= 1;
 	end
 end
 
+assign if_ex_jready = R_handshaked; // handshake with exu when R_handshaked
+assign avalid = if_ica_avalid;
+assign raddr = pc;
+assign rready = ~if_id_valid | (if_id_valid & id_if_ready); // ready when no if_id data is pending
+
+/*---------------------DPI-C--------------------*/
 function int read_inst();
-	return R_handshaked ? inst_fetch : inst_fetch_r;
+	return inst_fetch;
 endfunction
 
 export "DPI-C" function read_inst;
