76e3adc8c08ade31deeac598b075db68d2c0924a
tracer-ysyx <tracer@ysyx.org>
2026-06-15 12:28:32 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  12:28:32 up  1:24,  4 users,  load average: 0.42, 0.31, 0.33

--- npc/csrc/cpu/difftest/dut.c
+++ npc/csrc/cpu/difftest/dut.c
@@ -99,7 +99,7 @@ void difftest_step(uint32_t pc, uint32_t npc) {
 		ref_difftest_exec(1);
 		ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
 
-		checkregs(&ref_r, pc);
+		checkregs(&ref_r, npc);
 	} else 
 		n_ignore_first = true;
 
