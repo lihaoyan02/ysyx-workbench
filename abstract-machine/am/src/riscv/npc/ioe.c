a11f36b8190a04ac6f4c91e44ffca2d970fd4697
tracer-ysyx <tracer@ysyx.org>
2026-06-21 20:02:38 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  20:02:38 up  8:31,  3 users,  load average: 0.57, 0.26, 0.09

--- abstract-machine/am/src/riscv/npc/ioe.c
+++ abstract-machine/am/src/riscv/npc/ioe.c
@@ -16,6 +16,8 @@ static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg
 static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
 static void __am_uart_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = false;  }
 
+static void test(void *buf) { panic("access test register"); }
+
 typedef void (*handler_t)(void *buf);
 static void *lut[128] = {
   [AM_TIMER_CONFIG] = __am_timer_config,
@@ -24,6 +26,7 @@ static void *lut[128] = {
   [AM_INPUT_CONFIG] = __am_input_config,
   [AM_INPUT_KEYBRD] = __am_input_keybrd,
   [AM_UART_CONFIG]  = __am_uart_config,
+  [AM_UART_RX     ] = test,
 	[AM_GPU_CONFIG  ] = __am_gpu_config,
 	[AM_GPU_FBDRAW  ] = __am_gpu_fbdraw,
 	[AM_GPU_STATUS  ] = __am_gpu_status,
