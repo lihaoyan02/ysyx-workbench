c3c1a401d60ff4ac1a075dcab7ba76ebce79890a
tracer-ysyx <tracer@ysyx.org>
2026-06-21 19:05:42 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  19:05:42 up  7:34,  3 users,  load average: 0.73, 0.45, 0.37

--- abstract-machine/am/src/riscv/npc/ioe.c
+++ abstract-machine/am/src/riscv/npc/ioe.c
@@ -14,7 +14,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *);
 
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
 static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
-static void __am_uart_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = false;  }
+static void __am_uart_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
 
 typedef void (*handler_t)(void *buf);
 static void *lut[128] = {
@@ -34,7 +34,7 @@ static void fail(void *buf) { panic("access nonexist register"); }
 bool ioe_init() {
   for (int i = 0; i < LENGTH(lut); i++)
     if (!lut[i]) lut[i] = fail;
-	// __am_gpu_init();
+	__am_gpu_init();
   __am_timer_init();
   return true;
 }
