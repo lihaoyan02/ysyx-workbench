0d9b762c5903384b6a85a6ed3bef7eaf84abe32f
tracer-ysyx <tracer@ysyx.org>
2026-06-26 22:58:51 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  22:58:51 up  4:56,  4 users,  load average: 0.51, 0.43, 0.29

--- npc/vsrc/EXU.v
+++ npc/vsrc/EXU.v
@@ -126,10 +126,10 @@ assign exu_bypass_rs1 = (id_ex_rs1 != 0 & ex_ls_valid) & ((id_ex_rs1==ex_ls_rd) 
 assign exu_bypass_rs2 = (id_ex_rs2 != 0 & ex_ls_valid) & ((id_ex_rs2==ex_ls_rd) & (ex_ls_wb_ctrl==`WB_ALU));
 assign wbu_bypass_rs1 = (id_ex_rs1 != 0 & ls_wb_valid) & (id_ex_rs1==wb_rf_rd);
 assign wbu_bypass_rs2 = (id_ex_rs2 != 0 & ls_wb_valid) & (id_ex_rs2==wb_rf_rd);
-assign id_ex_rs1_data_bypass = exu_bypass_rs1 ? ex_ls_data_out : 
-							wbu_bypass_rs1 ? wb_rf_data : id_ex_rs1_data;
-assign id_ex_rs2_data_bypass = exu_bypass_rs2 ? ex_ls_data_out : 
-							wbu_bypass_rs2 ? wb_rf_data : id_ex_rs2_data;
+assign id_ex_rs1_data_bypass = exu_bypass_rs1 ? ex_ls_data_out : id_ex_rs1_data;
+							// wbu_bypass_rs1 ? wb_rf_data : id_ex_rs1_data;
+assign id_ex_rs2_data_bypass = exu_bypass_rs2 ? ex_ls_data_out : id_ex_rs2_data;
+							// wbu_bypass_rs2 ? wb_rf_data : id_ex_rs2_data;
 
 /*-------------------ALU----------------------*/
 wire [XLEN-1:0] op1;
