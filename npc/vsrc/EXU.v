3c971330e5a898c55d403d01d2728db1a2d6a5da
tracer-ysyx <tracer@ysyx.org>
2026-06-27 10:42:26 +0800

    > sim RTL ysyx_25120308 Li Haoyan Linux ysyx 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  10:42:26 up  2:34,  3 users,  load average: 1.28, 0.60, 0.27

--- npc/vsrc/EXU.v
+++ npc/vsrc/EXU.v
@@ -133,8 +133,8 @@ assign id_ex_rs2_data_bypass = exu_bypass_rs2 ? ex_ls_data_out :
 							wbu_bypass_rs2 ? wb_rf_data : id_ex_rs2_data;
 
 wire data_hazard;
-assign data_hazard = //((id_ex_rs1 != 0 & ex_ls_valid ) & (id_ex_rs1==wb_rf_rd) & (ex_ls_wb_ctrl==`WB_ALU)) |
-					//((id_ex_rs2 != 0 & ex_ls_valid) & (id_ex_rs2==wb_rf_rd) & (ex_ls_wb_ctrl==`WB_ALU)) |
+assign data_hazard = ((id_ex_rs1 != 0 & ex_ls_valid ) & (id_ex_rs1==wb_rf_rd) & (ex_ls_wb_ctrl != `WB_ALU)) |
+					((id_ex_rs2 != 0 & ex_ls_valid) & (id_ex_rs2==wb_rf_rd) & (ex_ls_wb_ctrl != `WB_ALU)) |
 					((id_ex_rs1 != 0 & lsu_bussy & (~ls_wb_valid)) & (id_ex_rs1==wb_rf_rd)) |
 					((id_ex_rs2 != 0 & lsu_bussy & (~ls_wb_valid)) & (id_ex_rs2==wb_rf_rd));
 /*-------------------ALU----------------------*/
